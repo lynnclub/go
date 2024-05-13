ffa7e387d417a170c74cb8c9bef8c89a7be1a5df
Lynn <lynncho@live.com>
2024-05-13 19:12:16 +0800

    Update pool.go

--- v1/pool/pool.go
+++ v1/pool/pool.go
@@ -5,16 +5,17 @@ import (
 )
 
 type Pool[T any] struct {
-	Pool   sync.Map
-	Create func(key any) *T
-	Close  func(key, value any) bool
-	mutex  sync.Mutex
+	Pool          sync.Map
+	mutex         sync.Mutex
+	Create        func(key any) *T
+	LockForCreate bool // 批量Create时建议加锁
+	Close         func(key, value any) bool
 }
 
 func (p *Pool[T]) Get(key any) *T {
 	if instance, ok := p.Pool.Load(key); ok {
 		return instance.(*T)
-	} else {
+	} else if p.LockForCreate {
 		p.mutex.Lock()
 		defer p.mutex.Unlock()
 		if instance, ok = p.Pool.Load(key); ok {
