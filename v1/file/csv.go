7aea7c5236c5601a17809b0fa701d9e7bcb143d9
Lynn <lynncho@live.com>
2024-10-28 21:38:59 +0800

    Update csv.go

--- v1/file/csv.go
+++ v1/file/csv.go
@@ -5,17 +5,34 @@ import (
 	"os"
 )
 
-func CSVWriter(filename string, headers ...string) *csv.Writer {
-	file, err := os.Create(filename + ".csv")
+func CSVWriter(filename string, overwrite bool, headers ...string) *csv.Writer {
+	var file *os.File
+	var err error
+
+	if overwrite {
+		file, err = os.Create(filename + ".csv") // 覆盖模式
+	} else {
+		file, err = os.OpenFile(filename+".csv", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	}
+
 	if err != nil {
 		panic(err)
 	}
 
 	writer := csv.NewWriter(file)
 
-	if len(headers) > 0 {
+	if overwrite || isEmpty(file) {
 		writer.Write(headers)
 	}
 
 	return writer
+}
+
+func isEmpty(file *os.File) bool {
+	stat, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	return stat.Size() == 0
 }
