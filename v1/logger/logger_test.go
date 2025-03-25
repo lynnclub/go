dcb99690fb1e9f6b296389a0c5851ade450831bf
Lynn <10293848+lynnclub@users.noreply.github.com>
2025-03-25 15:05:11 +0800

    FeishuAlert

--- v1/logger/logger_test.go
+++ v1/logger/logger_test.go
@@ -4,12 +4,12 @@ import (
 	"log"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/lynnclub/go/v1/datetime"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-// TestLog
 func TestLog(t *testing.T) {
 	Logger = New(
 		log.New(os.Stderr, "", log.Lmsgprefix),
@@ -40,4 +40,36 @@ func TestLog(t *testing.T) {
 	Info("321")
 	Debug("321")
 	Error("4321")
+}
+
+func TestSendDuplicateLog(t *testing.T) {
+	alert := &FeishuAlert{}
+	option := Option{
+		Levels:  []string{"ERROR"},
+		Webhook: "http://test.webhook",
+		SignKey: "test_sign_key",
+		UserId:  "test_user_id",
+	}
+
+	alert.Add("test", option)
+
+	log := LogEntry{
+		Level:     400,
+		LevelName: "ERROR",
+		Command:   "test_command",
+		Message:   "test_message",
+		Datetime:  time.Now().Format(time.RFC3339),
+		Trace:     "",
+		URL:       "",
+		Env:       "production",
+		IP:        "127.0.0.1",
+	}
+
+	alert.Send(log)
+
+	// 立即再次发送相同的日志，应该被过滤掉
+	alert.Send(log)
+	if len(alert.lastHashs) != 1 {
+		t.Errorf("Expected 1 log in history, got %d", len(alert.lastHashs))
+	}
 }
---
+++ v1/logger/send_feishu.go
@@ -0,0 +1,187 @@
+package logger
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/lynnclub/go/v1/algorithm"
+	"github.com/lynnclub/go/v1/array"
+	"github.com/lynnclub/go/v1/bytedance/feishu"
+	"github.com/lynnclub/go/v1/elasticsearch"
+)
+
+var Feishu *FeishuAlert
+
+func NewFeishu(options map[string]interface{}) *FeishuAlert {
+	instance := &FeishuAlert{}
+	instance.AddMapBatch(options)
+
+	return instance
+}
+
+type FeishuAlert struct {
+	options   map[string]Option
+	lastHashs []lastHash // 摘要
+	mutex     sync.Mutex
+}
+
+type lastHash struct {
+	hash string
+	time time.Time
+}
+
+type Option struct {
+	Levels    []string `json:"levels"`
+	Webhook   string   `json:"webhook"`
+	SignKey   string   `json:"sign_key"`
+	UserId    string   `json:"user_id"`
+	KibanaUrl string   `json:"kibana_url"`
+	EsIndex   string   `json:"es_index"`
+}
+
+func (f *FeishuAlert) Add(name string, option Option) {
+	if option.Webhook == "" {
+		panic("Option webhook empty " + name)
+	}
+
+	if f.options == nil {
+		f.options = make(map[string]Option)
+	}
+
+	f.options[name] = option
+}
+
+func (f *FeishuAlert) AddMap(name string, setting map[string]interface{}) {
+	levels := []string{}
+	if tmps, ok := setting["levels"].([]string); ok {
+		for _, level := range tmps {
+			levels = append(levels, strings.ToUpper(level))
+		}
+	}
+
+	option := Option{
+		Levels:    levels,
+		Webhook:   setting["webhook"].(string),
+		SignKey:   setting["sign_key"].(string),
+		UserId:    setting["user_id"].(string),
+		KibanaUrl: setting["kibana_url"].(string),
+		EsIndex:   setting["es_index"].(string),
+	}
+
+	f.Add(name, option)
+}
+
+func (f *FeishuAlert) AddMapBatch(batch map[string]interface{}) {
+	for name, setting := range batch {
+		f.AddMap(name, setting.(map[string]interface{}))
+	}
+}
+
+func (f *FeishuAlert) FindOption(levelName string, entry, defaultName string) string {
+	for name, option := range f.options {
+		if strings.Contains(entry, name) && (len(option.Levels) == 0 || array.In(option.Levels, levelName)) {
+			return name
+		}
+	}
+
+	return defaultName
+}
+
+func (f *FeishuAlert) Send(log LogEntry) {
+	if log.Level <= 200 {
+		return
+	}
+
+	name := ""
+	if log.URL == "" {
+		name = f.FindOption(log.LevelName, log.Command, "default_command")
+	} else {
+		name = f.FindOption(log.LevelName, log.URL, "default_api")
+	}
+
+	option, ok := f.options[name]
+	if !ok {
+		return
+	}
+
+	keyword := ""
+	if traces, ok := log.Extra.([]string); ok && len(traces) > 0 {
+		keyword = traces[0]
+	} else {
+		keyword = log.Command + log.Message
+	}
+
+	if keyword != "" {
+		newHash := lastHash{
+			hash: algorithm.MD5(keyword),
+			time: time.Now(),
+		}
+
+		for _, lastHash := range f.lastHashs {
+			if lastHash.hash == newHash.hash {
+				if lastHash.time.Add(10 * time.Minute).After(newHash.time) {
+					return
+				}
+			}
+		}
+
+		f.mutex.Lock()
+
+		f.lastHashs = append(f.lastHashs, newHash)
+		if len(f.lastHashs) > 10 {
+			f.lastHashs = f.lastHashs[1:]
+		}
+
+		f.mutex.Unlock()
+	}
+
+	content := map[string]interface{}{
+		"tag":  "text",
+		"text": f.Format(log, option.KibanaUrl, option.EsIndex),
+	}
+	feishu.NewGroupRobot(option.Webhook, option.SignKey).SendRich("", content, option.UserId)
+}
+
+func (f *FeishuAlert) Format(log LogEntry, kibanaUrl, esIndex string) string {
+	querys := []string{
+		elasticsearch.GetKuery("message", log.Message),
+	}
+
+	traceParam := ""
+	if log.Trace == "" {
+		traceParam = elasticsearch.GetKuery("command", log.Command)
+		log.Trace = log.Command
+	} else {
+		traceParam = elasticsearch.GetKuery("trace", log.Trace)
+	}
+
+	querys = append(querys, traceParam)
+
+	return fmt.Sprintf(`环境：%s
+级别：%s
+时间：%s
+IP：%s
+追踪：%s
+入口：%s
+
+%s
+
+详情
+%s
+链路
+%s
+
+如有问题请尽快处理 []~(￣▽￣)~*`,
+		log.Env,
+		log.LevelName,
+		log.Datetime,
+		log.IP,
+		log.Trace,
+		log.Command,
+		log.Message,
+		elasticsearch.GetKibanaUrl(kibanaUrl, esIndex, querys),
+		elasticsearch.GetKibanaUrl(kibanaUrl, esIndex, []string{traceParam}),
+	)
+}
